6365f838d0bcc57928ccf155c00263feb20aba6a
shun <shun.sugiyama@jp21.com>
2019-09-27 06:06:15 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(name="domextract",
       url="https://github.com/sugiyamath/domextract",
       packages=find_packages(),
       install_requires=[
-          "numpy==1.14.3",
-          "scikit-learn==0.19.1",
-          "pandas==0.23.0",
+          "numpy==1.17.1",
+          "scikit-learn==0.21.3",
+          "pandas==0.25.1",
           "bs4==0.0.1",
-          "mecab-python3==0.7"
+          "mecab-python3==0.996.1"
       ],
       package_data={
           'domextract':["*"]
